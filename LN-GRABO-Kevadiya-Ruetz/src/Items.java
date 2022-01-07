286ae885bd41079fa72b923f6854c08cc0638caa
gauextra <76999850+gauextra@users.noreply.github.com>
2022-01-07 10:47:06 +0100

    # WARNING: head commit changed in the meantime
    Merge remote-tracking branch 'origin/main' into Gautam
    
    Added Items and ListItems classes and Properties and methods into clases

---
+++ LN-GRABO-Kevadiya-Ruetz/src/Items.java
@@ -0,0 +1,27 @@
+
+public class Items {
+
+	//name of Item
+	 private String name;
+	  
+	
+	// preis of Item
+	private float preis = 0;
+
+	
+	//get Method for name of item
+	  public String getName() { 
+		  return name; 
+	  }
+	
+	//get method for pries of item
+	public float getPreis() {
+		return preis;
+	}
+
+
+	
+
+
+
+}
---
+++ LN-GRABO-Kevadiya-Ruetz/src/ListItems.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+
+/**
+ * @author kevad
+ *
+ */
+public class ListItems {
+
+	private String[] obst ;
+	private String[] gemuese ;
+	private String[] eis;
+	private String[] other;
+	
+	public String[] getObstItems() {
+		return obst;
+	}
+	public String[] getGemueseItems() {
+		return gemuese;
+	}
+	
+	public String[] getEisItems() {
+		return eis;
+	}
+	
+	public String[] getOtherItems() {
+		return other;
+	}
+	
+	
+	
+}
