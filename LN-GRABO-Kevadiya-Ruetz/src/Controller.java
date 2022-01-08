3f19d8f7a2573545f6b6e63ffc8a3ede7cb40112
Christian Ruetz <chrissi.ruetz@freenet.de>
2022-01-08 18:06:26 +0100

    ButtonAction-Handler hinzugefügt und Controller-Klasse in FXML verlinkt.
    Beim Drücken verschiedener Buttons werden nun verschiedene ActionHandler
    aufgerufen, welche Funktionalitäten zu den Buttons bereitstellen.

--- LN-GRABO-Kevadiya-Ruetz/src/Controller.java
+++ LN-GRABO-Kevadiya-Ruetz/src/Controller.java
@@ -1,5 +1,7 @@
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,7 +10,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class Controller implements Initializable {
-
+	//	ListItems lists;
+	
+	//	Item[] selectedList = lists.getObst();
+	
+	//mode 0: default, mode 1 :rückgeld eintragen
+		int mode = 0;
+	@FXML
+	private ListView selectedItems;
+	@FXML
+	private Button abkassieren;
+	@FXML
+	private Button plus;
+	@FXML
+	private Button minus;
 	
 	
 	@Override
@@ -17,14 +32,55 @@ public class Controller implements Initializable {
 		
 		//BlaBla
 	}
-
-	private Button cashUp;
 	
-	public void handeButtonAction(ActionEvent e) {
 	
-		System.out.println("ok passt");
+	public void itemAction(ActionEvent e) {
 		
-	};
+		System.out.println("Item button pressed");
+		Button src = (Button)e.getSource();
+		
+		
+		
+
+		
+	}
+
+	//Behandle klicks auf funktionale Buttons
+	public void functionAction(ActionEvent e) {
+		Button src = (Button)e.getSource();
+		
+		
+	}
+	
+	//Ändere selectedList bei wechseln des Tabs
+	public void tabAction(ActionEvent e) {
+		Tab src = (Tab)e.getSource();
+		
+		switch (src.getText()) {
+
+		case "Obst":
+		
+			//selectedList = lists.getObst(); 
+			break;
+		case "Gemuese":
+			
+			//selectedList = lists.getGemuese();
+			break;
+		case "Eis":
+			
+			//selectedList = lists.getEis();
+			break;
+		case "Other":
+			
+			//selectedList = lists.getOther();
+			break;
+			
+		default:
+			break;
+		
+		}
+		
+	}
 	
 	
 	
