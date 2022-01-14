b5ccf626eef7d0365d2a855d4c0cab5e192015ad
gauextra <76999850+gauextra@users.noreply.github.com>
2022-01-14 08:25:55 +0100

    each tab has 3x3 items. bigger buttons. change name in listItems

--- LN-GRABO-Kevadiya-Ruetz/src/ListItems.java
+++ LN-GRABO-Kevadiya-Ruetz/src/ListItems.java
@@ -8,25 +8,41 @@
  */
 public class ListItems {
 
-	private static Items[] obst = {new Items("Apple", 2.5), new Items("Watermelon", 2.0),
-							new Items("Strawberry", 2.0), new Items("Kiwi", 2.0),
-							new Items("Mango", 2.0), new Items("Avacado", 2.0),
-							new Items("Orange", 2.0), new Items("Banana", 2.0),
-							new Items("Grape", 2.0)} ;
-	private Items[] gemuese = {	new Items("Bohne", 2.0),new Items("Kohl", 2.0),
-								new Items("Karotte", 2.0),new Items("Bluemenkohl", 2.0),
-								new Items("Main", 2.0),new Items("Gurke", 2.0),
-								new Items("Kartoffeln", 2.0),new Items("Zwiebel", 2.0),
-								new Items("Tomate", 2.0)} ;
-	private Items[] eis = 	{new Items("Chocolate", 2.0),new Items("Strawberry", 2.0),
-								new Items("Vanilla", 2.0),new Items("EspressoFlake", 2.0),
-								new Items("BlackCherry", 2.0),new Items("PistachioAlmonds", 2.0),
-								new Items("FrenchVanilla", 2.0),new Items("MindChocolate", 2.0),
-								new Items("Coconut", 2.0)} ;
-	private Items[] other = {	new Items("Kitkat", 2.0),new Items("Mars", 2.0),
-								new Items("M & M", 2.0),new Items("Candy", 2.0),
-								new Items("RitterSport ", 2.0),new Items("OrangeSaft", 2.0),
-								new Items("Cola", 2.0),new Items("Pizza", 2.0),
+	private Items[] obst = 	{	new Items("Apfel", 2.5), 
+								new Items("Melone", 2.0),
+								new Items("Erdbeere", 2.0), 
+								new Items("Kiwi", 2.0),
+								new Items("Mango", 2.0), 
+								new Items("Avacado", 2.0),
+								new Items("Orange", 2.0), 
+								new Items("Banana", 2.0),
+								new Items("Ananas", 2.0)} ;
+	private Items[] gemuese = {	new Items("Bohne", 2.0),
+								new Items("Kohl", 2.0),
+								new Items("Karotte", 2.0),
+								new Items("Bluemenkohl", 2.0),
+								new Items("Gurke", 2.0),
+								new Items("Kartoffeln", 2.0),
+								new Items("Zwiebel", 2.0),
+								new Items("Tomate", 2.0),
+								new Items("Zitrone", 2.0)} ;
+	private Items[] eis = 	{	new Items("Chocolate", 2.0),
+								new Items("Vanilla", 2.0),
+								new Items("EspressoFlake", 2.0),
+								new Items("BlackCherry", 2.0),
+								new Items("Pistachio", 2.0),
+								new Items("FrenchVanilla", 2.0),
+								new Items("MindChocolate", 2.0),
+								new Items("Coconut", 2.0),
+								new Items("Mango", 2.0)} ;
+	private Items[] other = {	new Items("Kitkat", 2.0),
+								new Items("Mars", 2.0),
+								new Items("M & M", 2.0),
+								new Items("Candy", 2.0),
+								new Items("RitterSport", 2.0),
+								new Items("OrangeSaft", 2.0),
+								new Items("Cola", 2.0),
+								new Items("Pizza", 2.0),
 								new Items("Bier", 4.0)};
 	
 	public Items[] getObstItems() {
